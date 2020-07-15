d59bf3204b65917c5431bb70977b029d74534c50
robertjallen <bobbidigi34@gmail.com>
2020-07-15 19:12:28 -0400

    add fail test

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -14,7 +14,17 @@ function succeed(item) {
 }
 
 function fail(item) {
-  return { ...item };
+  if (item.enhancement < 15) {
+    return { ...item, durability: item.durability - 5 };
+  } else if (item.enhancement === 15 || item.enhancement === 16) {
+    return { ...item, durability: item.durability - 10 };
+  } else {
+    return {
+      ...item,
+      durability: item.durability - 10,
+      enhancement: item.enhancement - 1,
+    };
+  }
 }
 
 function repair(item) {
