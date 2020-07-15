31e6949766594d20595354f62a8b6c0d5ab08286
robertjallen <bobbidigi34@gmail.com>
2020-07-14 22:43:27 -0400

    add repairs

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,16 @@
 const enhancer = require('./enhancer.js');
 // test away!
+describe("enhancer unit tests", () => {
+  // repair durability
+	test('repairs', () => {
+    item = {
+      name: 'Gary',
+      durability: 19,
+      points: 1500,
+    };
+    const repairedItem = enhancer.repair(item);
+    expect(repairedItem.durability).toBe(100);
+  });
+
+
+})
